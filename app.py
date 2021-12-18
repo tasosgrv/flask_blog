1df7604c623125c5068b0d7f76e765be8b96413a
tasosgrv <tasosgr4@gmail.com>
2021-12-18 13:54:28 +0200

    V2.1.4 Routes And Route Variables

--- app.py
+++ app.py
@@ -2,18 +2,22 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/index')
 @app.route('/')
 def root():
-    return '''
-    <htnl>
-    <head><title>Τιτλος</title></head>
-    <body>
-    <h3>hello flask</h3>
-    </body>
-    </html>
-    '''
+    return  "<h3>hello flask</h3>"
     
+@app.route('/hello')
+def hello():
+    return "hello"
 
+@app.route('/hello/<name>')
+def helloName(name):
+    return f"hello {name}"
+
+@app.route('/id/<int:name>')
+def id(name):
+    return f"id {name}"
 
 if __name__=="__main__":
-    app.run()
+    app.run(debug=True)
