f8206c544cb9e6c01923d2a4177295991b2e6a7a
tasosgrv <tasosgr4@gmail.com>
2022-01-03 17:35:07 +0200

    V3.2.1 Φόρμες

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ def root():
     return  render_template('index.html')
 
 
-@app.route('/signup/')
+@app.route('/signup/', methods=['GET', 'POST'])
 def signup():
     return render_template('signup.html')
 
