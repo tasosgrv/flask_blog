50d24475d305d344f77bdc3dde47566bb0329fea
tasosgrv <tasosgr4@gmail.com>
2022-01-03 17:06:59 +0200

    V3.1.3 Δημιουργία βασικών σελίδων (routes/templates) του Blog μας

--- src/app.py
+++ src/app.py
@@ -1,6 +1,4 @@
-from abc import abstractmethod
-from flask import Flask, render_template
-import json
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
@@ -10,8 +8,22 @@ def root():
     return  render_template('index.html')
 
 
+@app.route('/signup/')
+def signup():
+    return render_template('signup.html')
 
-    
+@app.route('/login/')
+def login():
+    return render_template('login.html')
+
+@app.route('/logout/')
+def logout():
+    return redirect(url_for('root'))
+
+@app.route('/new_article/')
+def new_article():
+    return render_template('new_article.html')
+  
 
 
 if __name__=="__main__":
