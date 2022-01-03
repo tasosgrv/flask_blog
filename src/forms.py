77953bb43931db650ace5ab45aac1ae0ec0ff62d
tasosgrv <tasosgr4@gmail.com>
2022-01-03 17:59:47 +0200

    V3.2.3 WTForms: μέρος 1ο, εγκατάσταση και αρχικοποίηση

---
+++ src/forms.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+
+class SignupForm(FlaskForm):
+    username = StringField(label='Username')
+    email= StringField(label='email')
+    password= StringField(label='password')
+    password2= StringField(label='Επιβεβαίωση password')
+    submit= SubmitField('Εγγραφή')
