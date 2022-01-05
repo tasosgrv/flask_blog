d2b9d2932b4484de9cb6b69e0f8c97851a73694c
tasosgrv <tasosgr4@gmail.com>
2022-01-05 16:30:11 +0200

    V3.3.1 WTForms: Validators

--- src/forms.py
+++ src/forms.py
@@ -1,9 +1,24 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, PasswordField, EmailField, SubmitField
+from wtforms.validators import DataRequired, Email, Length, EqualTo
+
 
 class SignupForm(FlaskForm):
-    username = StringField(label='Username')
-    email = StringField(label='email')
-    password = StringField(label='password')
-    password2 = StringField(label='Επιβεβαίωση password')
+    username = StringField(label='Username', 
+                            validators=[DataRequired(message="Αυτό το πεδίο δεν μπορεί να ειναι κενό"), 
+                                        Length(min=3, max=15, message="To username πρέπει να είναι απο 3-15 χαρακτηρες")]
+                            )
+    email = EmailField(label='email', 
+                        validators=[DataRequired(message="Αυτό το πεδίο δεν μπορεί να ειναι κενό"),
+                                    Email(message="Παρακαλώ εισάγετε ένα σωστό email")]
+                        )
+    password = PasswordField(label='password', 
+                            validators=[DataRequired(message="Αυτό το πεδίο δεν μπορεί να ειναι κενό"), 
+                                        Length(min=3, max=15, message="To password πρέπει να είναι απο 3-15 χαρακτηρες")]
+                            )
+    password2 = PasswordField(label='Επιβεβαίωση password', 
+                            validators=[DataRequired(message="Αυτό το πεδίο δεν μπορεί να ειναι κενό"), 
+                                        Length(min=3, max=15, message="To password πρέπει να είναι απο 3-15 χαρακτηρες"), 
+                                        EqualTo('password', message="Τα δυο πεδία password πρέπει να είναι ίδια")]
+                            )
     submit = SubmitField('Εγγραφή')
